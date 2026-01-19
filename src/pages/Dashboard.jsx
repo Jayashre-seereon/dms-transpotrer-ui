751e946a6ea18b48dc1f6d7a29d2d774beabe8ce
jayashreeseereon <jayashree.seereon@gmail.com>
2026-01-19 11:47:24 +0530

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,10 +14,10 @@ import {
 
 // ---------------- Data ----------------
 const summary = [
-  { label: "Total Value", value: 150, icon: <DollarOutlined className="text-amber-600!" /> },
+  { label: "Total Assignments", value: 150, icon: <DollarOutlined className="text-amber-600!" /> },
   { label: "Pending Assignments", value: 124, icon: <ClockCircleOutlined className="text-yellow-600!" /> },
-  { label: "In Transit", value: 121, icon: <TruckOutlined className="text-blue-600!" /> },
-  { label: "Delivered", value: 123, icon: <CalendarOutlined className="text-green-600!" /> },
+  { label: "In Transit Deliveries", value: 121, icon: <TruckOutlined className="text-blue-600!" /> },
+  { label: "Delivered Orders", value: 123, icon: <CalendarOutlined className="text-green-600!" /> },
 ];
 
 
@@ -56,11 +56,11 @@ const recentShipments = [
 // ---------------- Reusable Components ----------------
 
 const SummaryCard = ({ label, value, icon }) => (
-  <Card className="rounded-xl shadow-sm border border-amber-200 w-60">
+  <Card className="rounded-xl! shadow-sm! border! border-amber-200! w-60!">
     <div className="flex flex-col gap-2">
       <p className="text-amber-500 text-base font-medium">{label}</p>
 
-      <div className="flex items-center gap-30">
+      <div className="flex items-center gap-30 ">
         {React.cloneElement(icon, { className: "text-amber-700 text-xl" })}
         <p className="text-lg font-semibold text-amber-700 truncate">
           {label === "Total Value" ? `₹${(+value).toLocaleString()}` : value}
